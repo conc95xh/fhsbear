c5ba118cd97f64b57bb16c0b4f6427f7d1ff38c0
kh940793 <kaihan.huang@broadcom.com>
2026-03-19 11:46:43 +0800

    flake.nix: iwhtout using profile and still can support python package
    Change-Id: I9df5c6e01c1c708d5b02995a3b3c3341128e6da2

--- flake.nix
+++ flake.nix
@@ -2,27 +2,33 @@
   description = "FHS environment for Broadcom BDK and Bear";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-cmake.url = "github:NixOS/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";
+    #nixpkgs-gcc.url = "github:NixOS/nixpkgs/057f9aecfb71c4437d2b27d3323df7f93c010b7e"; # Pin a legacy branch
   };
 
-  outputs = { self, nixpkgs,nixpkgs-cmake }:
+  outputs = { self, nixpkgs}:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      pkgs-cmake = nixpkgs-cmake.legacyPackages.${system};
+      pythonEnv = pkgs.python313.withPackages (ps: with ps; [ 
+        packaging 
+        # add other modules here if needed
+      ]);
     in
     {
       devShells.${system}.default = (pkgs.buildFHSEnv {
         name = "broadcom-fhs";
+        extraOutputsToInstall = [ "dev" "static" ];
         targetPkgs = pkgs: with pkgs; [
-          pkgs-cmake.cmake
+          cmake
+          gcc
+          glibc
           gettext
           popt
           flex
           libtool
           gnutar
-          libuuid.dev
           libuuid
           lzo
           pkg-config
@@ -41,30 +47,44 @@
           rsync
           git
           openssl
-          openssl.dev
           automake
           bison
           bear
           gnumake
           bash
-          glibc
-          gcc.cc.lib
-          zlib.dev
-          ncurses.dev
+          zlib
+          file
+          libxcrypt
           ncurses
           util-linux
           hostname
           which
           perl
-          python313
-          python313Packages.packaging # Add it as a standalone package
-
+	  pythonEnv
+          binutils
+          git
+          # --- Modern System Utilities ---
+          bat       # modern 'cat'
+          eza       # modern 'ls'
+          fd        # modern 'find'
+          ripgrep   # modern 'grep'
+          zoxide    # modern 'cd'
+          btop      # system monitor
+          dust   # disk usage (dust)
+          jq        # JSON processor
+            
+          # --- Connectivity & Editing ---
+          curl
+          wget
         ];
         
         # This is the magic part:
         # It sets up the environment variables inside the FHS shell
+        #profile = ''
+        #  export PYTHONPATH="${pkgs.python313Packages.packaging}/lib/python3.13/site-packages:$PYTHONPATH"
+        #'';
         profile = ''
-          export PYTHONPATH="${pkgs.python313Packages.packaging}/lib/python3.13/site-packages:$PYTHONPATH"
+          export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/share/pkgconfig:$PKG_CONFIG_PATH"
         '';
 
         runScript = "bash";
